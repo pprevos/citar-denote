4586e030445ff40a4939fe0ecc1aae667825052d
Peter Prevos <peter@prevos.net>
2024-05-05 10:34:10 +1000

    Version 2.2.1

--- citar-denote.el
+++ citar-denote.el
@@ -5,7 +5,7 @@
 ;; Author: Peter Prevos <peter@prevos.net>
 ;; Maintainer: Peter Prevos <peter@prevos.net>
 ;; Homepage: https://github.com/pprevos/citar-denote
-;; Version: 2.2
+;; Version: 2.2.1
 ;; Package-Requires: ((emacs "28.1") (citar "1.4") (denote "2.0") (dash "2.19.1"))
 
 ;; This file is NOT part of GNU Emacs.
@@ -28,21 +28,14 @@
 ;; A minor-mode integrating 'citar' and 'denote' to create and manage
 ;; bibliographic notes.
 ;;
-;; Taking notes about the articles and books you read is essential to
-;; intellectual life.  Many note-taking systems can connect to a
-;; bibliographic database (such as a plain text BibTeX file or external
-;; software such as Zotero).  These databases are helpful when you need to
-;; add citations to your writing.  Electronic bibliographies also help you
-;; create and find notes related to your literature collection and provide
-;; access to related materials, such as online sources and files stored on
-;; your computer.
-
 ;; Citar-Denote integrates the Citar bibliography package with the Denote
 ;; note-taking system.  Citar lets you browse and act on bibliographic data
 ;; in BibTeX, BibLaTeX or JSON format.  Denote is a versatile Emacs package
 ;; for taking and managing notes. Combining these two packages enables
 ;; linking notes in your Denote collection to your bibliography, providing
 ;; a complete solution for documenting literature reviews.
+;;
+;; Read the manual with `C-h R citar-denote`.
 
 ;;; Code:
 
